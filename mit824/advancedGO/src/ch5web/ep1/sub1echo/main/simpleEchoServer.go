6635edaa417b69459d25a4d17cc2ade98f4c43f2
Junyao Wang <junwow@amazon.com>
2018-08-27 15:10:12 -0700

    starting ch5 web

---
+++ mit824/advancedGO/src/ch5web/ep1/sub1echo/main/simpleEchoServer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"io/ioutil"
+)
+
+func echo(wr http.ResponseWriter, r *http.Request) {
+	msg, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		wr.Write([]byte("echo error"))
+		return
+	}
+
+	writeLen, err := wr.Write(msg)
+	if err != nil || writeLen != len(msg) {
+		log.Println(err, "write len:", writeLen)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", echo)
+	err := http.ListenAndServe(":5050", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
